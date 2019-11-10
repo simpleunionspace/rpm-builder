de8b21cd179ffa6fd6186baa46f2567ccfab6bf4
Pavel Kurkin <kurkinps@kurkin-family.net>
2019-11-11 00:01:56 +0300

    dev: removed mount tmp directory on build

--- make/make.ps1
+++ make/make.ps1
@@ -6,5 +6,4 @@ docker run `
        -it `
        -v ${BasePath}/../src/${SpecName}:/opt/builder/in `
        -v ${BasePath}/../rpm/${SpecName}:/opt/builder/out `
-       -v ${BasePath}/../tmp/${SpecName}:/root/rpmbuild `
        lifepainspace/rpm-builder:latest
--- make/make.sh
+++ make/make.sh
@@ -8,5 +8,4 @@ docker run `
        -it `
        -v $BASEPATH/../src/$SPEC:/opt/builder/in `
        -v $BASEPATH/../rpm/$SPEC:/opt/builder/out `
-       -v $BASEPATH/../tmp/$SPEC:/root/rpmbuild `
        lifepainspace/rpm-builder:latest
